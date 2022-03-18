c06b0c19965c3120d7119b6a4c4978a3150d54a9
Iris Leal <iris_leal_m@hotmail.com>
2022-03-17 20:52:56 -0700

    Now it deletes using the API.

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,12 @@
 import axios from "axios";
-import React  from "react";
+import React, {useState}  from "react";
 
+// TODO add spred arg ... to accept the args. ...task, location
 function TaskItem(props){
     const API_SERVER_BASE = process.env.REACT_APP_API_SERVER_BASE
     const API_SERVER_DELETE = process.env.REACT_APP_API_SERVER_DELETE
+    const API_SERVER_UPDATE = process.env.REACT_APP_API_SERVER_PUT
+    const [location, setLocation] = useState(props.task.location)
 
 
     const deleteTaskHandler = (task) => {
@@ -12,20 +15,38 @@ function TaskItem(props){
         .catch(error => console.error(error))
     // .finally(() => console.log('done'));
     }
+
+    const updateTaskHandler = (task) =>{
+        axios.put(API_SERVER_BASE + API_SERVER_UPDATE + `${task}?location=${location}`)
+        .then(res => console.log(res.data))
+        .catch(error => console.error(error))
+    }
+
     return(
         <div>
             <p>
-            path: {API_SERVER_BASE + API_SERVER_DELETE + props.task.task}
-            </p>
-            <p>
                 <span style={{'fontWeight':'bold, underline'}}>
-                    {props.task.task} :  </span>
-                {props.task.location}
+                    Task: 
+                    <input className='mb-2 form-control taskIn' 
+                        value={props.task.task}
+                        disabled  />
+                <br/>                    
+                    Location: 
+                    <input className='mb-2 form-control taskIn' 
+                        value={location}                        
+                        onChange={event => setLocation(event.target.value)}  />
+                    </span>
+                <br/>
                 <button
                     onClick={() => deleteTaskHandler(props.task.task)}
                     className="btn btn-outline-danger my-2 mx-2"
                     style={{'borderRadius':'50px'}}    >
-                     X </button>
+                    X </button>
+                <button
+                    onClick={() => updateTaskHandler(props.task.task)}
+                    className="btn btn-outline-danger my-2 mx-2"
+                    style={{'borderRadius':'50px'}}    >
+                    Upd </button>
             </p>
             <hr></hr>
         </div>
